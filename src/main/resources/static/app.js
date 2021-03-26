f2e9258ffa09c6af38797d42acaf0ba99071a95d
Santiago Buitrago <brayan.buitrago@escuelaing.edu.co>
2021-03-26 16:30:38 -0500

    Laboratorio 8

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -15,6 +15,7 @@ var app = (function () {
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
+        stompClient.send("/topic/newpoint", {}, JSON.stringify(point));
     };
     
     
@@ -36,9 +37,8 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                            alert(JSON.parse(eventbody.body).x+","+JSON.parse(eventbody.body).y);
             });
         });
 
